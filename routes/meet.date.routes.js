9424858b3b8cb65cddc0ddc5fc619e60f7c4c7fc
kristinmc42 <mccollum.k.j@gmail.com>
2022-08-22 11:29:56 -0400

    refactored overlap calculations in /overlapping/:meetingNumber get request

--- routes/meet.date.routes.js
+++ routes/meet.date.routes.js
@@ -84,155 +84,248 @@ meetDateRoute.route("/results/:meetingNumber").get(async function (req, res) {
 
 meetDateRoute.route("/overlapping/:meetingNumber").get( async function (req, res) {
     const meetingNumber = req.params.meetingNumber;
-    const data = await MeetDateModel.find({ meetingNumber: meetingNumber }).lean()
-    const importantArray = data[0].availabilityArray
-
+    const data = await MeetDateModel.find({ meetingNumber: meetingNumber }).lean();
+    const arrayOfUsers = data[0].users;
+    console.log(`l89 - arrayOfUsers is ${arrayOfUsers}`)
+    let availabilityByDateArrays = [];
     
-    const createTimeSlots = (dayArray) => {
-        // get date from dayArray if length > 0
-        let dateString = "";
-        let timeArrayAmPm = [];
-        if(dayArray.length > 0){
-          let startTime = dayArray[0].availability[0].start;
-          const regEx = /(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/;
-          const datesFromObj = regEx.exec(startTime);
-          // console.log("------HERE------")
-          // console.log(datesFromObj[0])
-          // console.log("------HERE------")
-          dateString = datesFromObj[0];
-          // console.log("------HERE------")
-          // console.log(dateString)
-          // console.log("------HERE------")
-        let amPmTime = "";
-        let timeString = "";
-        // console.log("------HERE------")
-        // console.log(dateString)
-        // console.log("------HERE------")
-        for(let i = 0; i < 48; i++){
-            let counter = 0;
-            if ( i === 0 ){
-                amPmTime = "12:00 am - 12:30 am";
-                timeString = `${dateString}T00:00:00`;
-            } else {
-              if (i % 2 !== 0) {
-                  counter++;
-              }
+    // function to get date from time string, returns a time object 
+    const getDate = (timestring) => {
+      let dateObject = new Date(timestring);
+      let year = dateObject.getFullYear();
+      let month = dateObject.getMonth() + 1;
+      let date = dateObject.getDate();
+      let hours = dateObject.getHours();
+      let minutes = dateObject.getMinutes();
 
-              if (i < 9){
-                timeString = i % 2 === 0 ?`${dateString}T0${(i - counter)/2}:00:00` :`${dateString}T0${(i - counter)/2}:30:00`
+      if (month < 10){
+        month = `0${month}`;
+      }
+
+      if(date < 10){
+        date = `0${date}`;
+      }
+
+      let dateString = `${year}-${month}-${date}`
+
+      return {dateString: dateString, hours:hours, minutes: minutes}
+    }
+
+
+    // function that creates an array with 48 timeslots (30 min slots) for a specific date passed as parameter
+    const createTimeSlots = (dateString) => {
+      let amPmTime = "";
+      let timeString = "";
+      let timeArrayAmPm = [];
+
+      for(let i = 0; i < 48; i++){
+          let counter = 0;
+          if ( i === 0 ){
+              amPmTime = "12:00 am - 12:30 am";
+              timeString = `${dateString}T00:00:00`;
+          } else {
+            if (i % 2 !== 0) {
+                counter++;
+            }
+
+            if (i < 9){
+              timeString = i % 2 === 0 ?`${dateString}T0${(i - counter)/2}:00:00` :`${dateString}T0${(i - counter)/2}:30:00`
+            }else{
+              timeString = i % 2 === 0 ?`${dateString}T${(i - counter)/2}:00:00` :`${dateString}T${(i - counter)/2}:30:00`
+            }
+
+            if(i < 24){
+              if (i === 1){
+                amPmTime = "12:30 am - 1:00 am"
+              }else if (i === 23){
+                amPmTime = "11:30 am - 12:00 pm"
               }else{
-                timeString = i % 2 === 0 ?`${dateString}T${(i - counter)/2}:00:00` :`${dateString}T${(i - counter)/2}:30:00`
+                amPmTime =
+                i % 2 === 0 ? `${(i - counter) / 2}:00 am - ${(i - counter) / 2}:30 am` : `${(i - counter) / 2}:30 am - ${((i - counter) / 2) + 1}:00 am`;
               }
-    
-              if(i < 24){
-                if (i === 1){
-                  amPmTime = "12:30 am - 1:00 am"
-                }else if (i === 23){
-                  amPmTime = "11:30 am - 12:00 pm"
-                }else{
-                  amPmTime =
-                  i % 2 === 0 ? `${(i - counter) / 2}:00 am - ${(i - counter) / 2}:30 am` : `${(i - counter) / 2}:30 am - ${((i - counter) / 2) + 1}:00 am`;
-                }
+            }else{
+              if (i === 24){
+                amPmTime = "12:00 pm - 12:30 pm";
+              }else if ( i === 25){
+                amPmTime = "12:30 pm - 1:00 pm"
+              }else if( i === 47){
+                amPmTime = "11:30 pm - 12:00 am"
               }else{
-                if (i === 24){
-                  amPmTime = "12:00 pm - 12:30 pm";
-                }else if ( i === 25){
-                  amPmTime = "12:30 pm - 1:00 pm"
-                }else if( i === 47){
-                  amPmTime = "11:30 pm - 12:00 am"
-                }else{
-                  amPmTime =
-                  i % 2 === 0 ? `${((i - counter) / 2) - 12}:00 pm - ${((i - counter) / 2) -12}:30 pm` : `${((i - counter) / 2) - 12}:30 pm - ${(((i - counter) / 2) + 1) -12}:00 pm`;
-                }
+                amPmTime =
+                i % 2 === 0 ? `${((i - counter) / 2) - 12}:00 pm - ${((i - counter) / 2) -12}:30 pm` : `${((i - counter) / 2) - 12}:30 pm - ${(((i - counter) / 2) + 1) -12}:00 pm`;
               }
             }
-            // console.log(timeString)
-            // console.log("------HERE------")
-            // console.log(timeString)
-            // console.log("------HERE------")
-            timeArrayAmPm.push({ time: amPmTime, timeString: timeString, array: [] });
-            }
-        }
+          }
 
-        
-            return timeArrayAmPm;
+          timeArrayAmPm.push({ time: amPmTime, timeString: timeString, array: [] });
+      }
+      return timeArrayAmPm;
     }
 
-    let day0array = createTimeSlots(importantArray.sunday);
-    let day1array = createTimeSlots(importantArray.monday);
-    let day2array = createTimeSlots(importantArray.tuesday);
-    let day3array = createTimeSlots(importantArray.wednesday);
-    let day4array = createTimeSlots(importantArray.thursday);
-    let day5array = createTimeSlots(importantArray.friday);
-    let day6array = createTimeSlots(importantArray.saturday);
-
-    const addUserToDayArray = (dayArray, event) => {
-       
-        // get start and end for each event as Date object
-        // need to be turned into a date object
-        let startObj = new Date(event.availability[0].start);
-        let endObj = new Date(event.availability[0].end);
-        let start = 
-           startObj.getHours() * 2 + 
-           (startObj.getMinutes() === 0 ?0 :1);
-         let end =
-           endObj.getHours() * 2 +
-           (endObj.getMinutes() === 0 ? 0 : 1);
-         // add user to day1results for the timeblock start and end
-         for(let i=start; i < end; i++){
-           dayArray[i].array.push({user: event.userName, start: start, end: end, startObj: startObj, endObj: endObj})
-         }
-        //  console.log(startObj)
-        //  console.log(endObj)
-         return dayArray;
-      }
-
-    if (importantArray.sunday.length > 0){
-        importantArray.sunday.forEach(timeblock => {
-            let sundayResults = addUserToDayArray(day0array, timeblock)
-            // console.log(sundayResults)
-    })
+    // function that adds user's availability block to the day array
+    const addUserToDayArray = (dayArray, startObj, endObj, userName) => {
+    // console.log(startObj, endObj)
+    let start = 
+      startObj.hours * 2 + 
+      (startObj.minutes === 0 ?0 :1);
+    let end =
+      endObj.hours * 2 +
+      (endObj.minutes === 0 ? 0 : 1);
+    // add user to day results for the timeblock start and end
+    for(let i=start; i < end; i++){
+      dayArray[i].array.push({user: userName, start: start, end: end, startObj: startObj, endObj: endObj})
     }
-    if (importantArray.monday.length > 0){
-        importantArray.monday.forEach(timeblock => {
-          let mondayResults = addUserToDayArray(day1array, timeblock)
-        //   console.log(mondayResults)
-        })
-      }
-    if (importantArray.tuesday.length > 0){
-        importantArray.tuesday.forEach(timeblock => {
-          let tuesdayResults = addUserToDayArray(day2array, timeblock)
-        //   console.log(tuesdayResults)
-        })
-      }
-    if (importantArray.wednesday.length > 0){
-        importantArray.wednesday.forEach(timeblock => {
-          let wednesdayResults = addUserToDayArray(day3array, timeblock)
-        //   console.log(wednesdayResults)
-        })
-      }
-    if (importantArray.thursday.length > 0){
-        importantArray.thursday.forEach(timeblock => {
-          let thursdayResults = addUserToDayArray(day4array, timeblock)
-        //   console.log(thursdayResults)
-        })
-      }
-    if (importantArray.friday.length > 0){
-        importantArray.friday.forEach(timeblock => {
-          let fridayResults = addUserToDayArray(day5array, timeblock)
-        //   console.log(fridayResults)
-        })
-      }
-    if (importantArray.saturday.length > 0){
-        importantArray.saturday.forEach(timeblock => {
-          let saturdayResults = addUserToDayArray(day6array, timeblock)
-        //   console.log(saturdayResults)
-        })
-    }
-    
 
-        // create day arrays and push the time array into the day array
-    return res.send({day0array, day1array, day2array, day3array, day4array, day5array, day6array});
+    return dayArray;
+    }
+
+  
+  
+    // function to add user's availability timeblocks
+    const fillTimeSlots = ({availabilityArray, userName}) => {
+
+      // function for replacing objects in the availabilityByDateArrays
+      const replaceExistingArray = (retrievedObject, startObj, endObj, userName, date, index) => {
+        console.log(`replaceExistingArray function called`)
+        console.log(retrievedObject, startObj, endObj, userName, date, index)
+        const updatedArray = addUserToDayArray(retrievedObject, startObj, endObj, userName);
+        const updatedObject = {date: date, availabilityByDateArray: updatedArray}
+        console.log(`l190 - updatedObject date is ${updatedObject.start}`)
+        // replace the array using the index of the retrievedObject
+        availabilityByDateArrays.splice(index, 1, updatedObject);
+      }
+
+      // function for creating a new object and adding availability
+      const createNewArray = (date, startObj, endObj, userName) => {
+        console.log(`createNewArray function called`)
+        const newDayArray = createTimeSlots(date);
+        const updatedDayArray = addUserToDayArray(newDayArray, startObj, endObj, userName);
+        console.log(`l202 - updatedDayArray length is ${updatedDayArray.length}`)
+        const updatedDayObject = {date: date, availabilityByDateArray: updatedDayArray};
+        console.log(`l204 - updatedDayObject date is ${updatedDayObject.date}`)
+        availabilityByDateArrays.push(updatedDayObject);
+      }
+
+      if(availabilityArray.length > 0){
+          // iterate through user's availability 
+          availabilityArray.forEach((availObj) => {
+            
+              const startObj = getDate(availObj.start);
+              const startDate = startObj.dateString;
+              const endObj = getDate(availObj.end);
+              const endDate = endObj.dateString;
+
+              console.log(`availObj start is ${availObj.start}`)
+              console.log(`availabilityByDateArrays.length is ${availabilityByDateArrays.length}`)
+              // availabilityByDateArrays is an array of objects, object has date and availabilityByDateArray keys
+              if (availabilityByDateArrays.length > 0){
+                // availabilityByDateArrays contains objects 
+                // if it contains an object with the start date, find the object
+                const retrievedStartObject = availabilityByDateArrays.find(({date}) => date === startDate);
+                const indexOfRetrievedStartObject = availabilityByDateArrays.findIndex(object => object.date === startDate);
+              
+                // check for object with end date
+                const retrievedEndObject = availabilityByDateArrays.find(({date}) => date === endDate);
+                const indexOfRetrievedEndObject = availabilityByDateArrays.findIndex(object => object.date === endDate);
+
+                if (indexOfRetrievedStartObject !== -1 && startDate === endDate){
+                  // there is an object with a matching start and end date
+                  console.log("there is an object with a matching start and end date")
+                  // add this availability to the array
+                  
+                  replaceExistingArray(retrievedStartObject.availabilityByDateArray, startObj, endObj, userName, startDate, indexOfRetrievedStartObject);
+                }else if (startDate === endDate) {
+                  // start and end match but there is not an object with that date
+                  console.log("start and end date match but there is not an object with that date")
+                  // create new one
+                  createNewArray(startDate, startObj, endObj, userName);
+
+                }else if(indexOfRetrievedStartObject !== -1 && indexOfRetrievedEndObject !== -1){
+                  // there is an object that matches the start date and an object that matches the end date
+                  console.log(" is an object that matches the start date and an object that matches the end date")
+                  // add availability to the start date array at the start time and use end time of 24:00
+                  replaceExistingArray(retrievedStartObject.availabilityByDateArray, startObj, {hours:24, minutes:0}, userName, startDate, indexOfRetrievedStartObject);
+
+                  // and add availability on end date array starting at 0:00 and ending at end time
+                  replaceExistingArray(retrievedEndObject.availabilityByDateArray, {hours:0, minutes:0}, endObj, userName, endDate, indexOfRetrievedEndObject);
+
+                }else if(indexOfRetrievedStartObject !== -1) {
+                  // there is an object for start date but not end date
+                  console.log("there is an object for start date but not end date")
+                  // add availability to the start date array at the start time and use end time of 24:00
+                  replaceExistingArray(retrievedStartObject.availabilityByDateArray, startObj, {hours:24, minutes:0}, userName, startDate, indexOfRetrievedStartObject);
+
+                  // there is not an array for the date, need to create a new one
+                  createNewArray(endDate, {hours:0, minutes:0}, endObj, userName);
+
+                }else if (indexOfRetrievedEndObject !== -1){
+                  // there is an object for the end date but not the start date
+                  console.log("there is an object for the end date but not the start date")
+                  // and add availability on end date array starting at 0:00 and ending at end time
+                  replaceExistingArray(retrievedEndObject.availabilityByDateArray, {hours:0, minutes:0}, endObj, userName, endDate, indexOfRetrievedEndObject);
+
+                  // create new one for start date with end time of 24:00
+                  createNewArray(startDate, startObj, {hours:24, minutes:0}, userName);
+                }else{
+                  // there are no objects for the start or end date
+                  console.log("there are no objects for the start or end date")
+                  // create new one for start date with end time of 24:00
+                  createNewArray(startDate, startObj, {hours:24, minutes:0}, userName);
+                  // create new one with start time of 0:00
+                  createNewArray(endDate, {hours:0, minutes:0}, endObj, userName);
+                }
+              }else{
+                // the availabilityByDateArrays is empty
+                console.log("the availabilityByDateArrays is empty")
+                if(startDate === endDate){
+                  // create new one
+                  createNewArray(startDate, startObj, endObj, userName);
+                }else{
+                  // create new one for start date with end time of 24:00
+                  createNewArray(startDate, startObj, {hours:24, minutes:0}, userName);
+                  // create new one with start time of 0:00
+                  createNewArray(endDate, {hours:0, minutes:0}, endObj, userName);
+                }
+              }   
+          })
+          console.log(`l285 - availabilityByDateArrays length is ${availabilityByDateArrays.length}`)
+         
+      }
+      else{
+        console.log(`l287 - availabilityArray.length is not greater than 0`)
+      }
+    } // end of fillTimeSlots function
+
+    // iterate through array of users
+    // call fillTImeSlots function to add each user's availability to the availabilityByDateArrays
+    try{
+      arrayOfUsers.forEach( user => {
+        const userName = user.userName;
+        console.log(`l294 - userName is ${userName}`)
+        const availabilityArray = user.availability;
+        console.log(`l299 - availabilityArray.length is ${availabilityArray.length}`)
+        fillTimeSlots({availabilityArray, userName});
+      });
+      // return the array containing all of the availability objects
+      if(availabilityByDateArrays !== undefined && availabilityByDateArrays.length > 0){
+        const sortedResults = availabilityByDateArrays.sort((a,b) => {
+          if(a.date < b.date){
+            return -1;
+          }
+          if(a.date > b.date){
+            return 1;
+          }
+        });
+        return res.send(sortedResults);
+      }
+    }catch{
+      console.log(`l309 - catch error while itterating through array of users `)
+    }
+  
+
+
+  
+
         
 })
 
