6eaf5aa170e98470d16a5860005cd076f85e584c
kristinmc42 <mccollum.k.j@gmail.com>
2023-03-10 13:20:36 -0500

    changed origin for cors

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["https://team-plan-it-app.herokuapp.com/", "https://afternoon-everglades-72307.herokuapp.com/", "https://plan-it.team/"]
-     
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
