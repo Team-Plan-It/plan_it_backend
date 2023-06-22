71236fadf39efcd55c236910a48b3359087e8c11
John Marcos <johnsaguay@gmail.com>
2023-06-22 18:11:08 -0400

    updated headers for server

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
 
 app.use("/dates", meetDateRoute);
 
